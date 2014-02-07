0be05f0d272d50ebb183b1400acfc1ae9dea583c
Max Chou <coderickl4m4r@gmail.com>
2014-02-07 01:39:02 -0800

    Correcting for loop - line 5
    You refer to string instead of lower_string. If you iterate across string and you come across a capitalized vowel, the program won't count it.

--- python/count_vowels.py
+++ python/count_vowels.py
@@ -2,7 +2,7 @@ def count_vowels(string):
 	vowels = ['a', 'e', 'i', 'o', 'u']
 	lower_string = string.lower()
 	vowel_count = 0
-	for char in string:
+	for char in lower_string:
 		for vowel in vowels:
 			if char == vowel:
 				vowel_count+=1
